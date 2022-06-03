2e30fc1bfb9492ebf169875a08caf12b5c71a56e
Bitcoinera <anagjordano@gmail.com>
2022-06-03 12:08:35 +0200

    add copyright symbol

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const currentYear = new Date().getFullYear();
 
 function Footer() {
     return <footer>
-        <p>Copyright {currentYear}</p>
+      <p>Copyright ⓒ {currentYear}</p>
     </footer>
 }
 
