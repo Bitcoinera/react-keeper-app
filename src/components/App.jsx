6d7f4d369cfee6891ad191651d0b4b88fbb7dae3
Bitcoinera <anagjordano@gmail.com>
2022-06-03 12:08:23 +0200

    upgrade

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 import Header from "./Header";
-import Note from "./Note";
 import Footer from "./Footer";
-
-import notes from "../notes";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
-    return <div>
-        <Header />
-        {notes.map((note) => {
-            return <Note 
-                key={note.key}
-                title={note.title}
-                content={note.content}
-            />
-        })}
-        <Footer />
+  return (
+    <div>
+      <Header />
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
+      <Footer />
     </div>
+  );
 }
 
 export default App;
